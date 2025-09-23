c1f194fa4da631c8981c149fad95f8caacd3646c
Sebastian <cancrosebastian@gmail.com>
2025-09-23 20:37:54 -0300

    Fix de articulos

--- API/src/Controller/Article/ArticleGetController.php
+++ API/src/Controller/Article/ArticleGetController.php
@@ -12,13 +12,11 @@ final readonly class ArticleGetController {
     public function start(int $id): void 
     {
         $article = $this->service->find($id);
-       
-        $MINIO_URL = "http://172.24.224.1:9000/imagenes/";
 
         echo json_encode([
             "id" => $article->id(),
             "title" => $article->title(),
-            "image" => $MINIO_URL . $article->image(),
+            "image" => $article->image(),
             "body" => $article->body(),
             "date" => $article->date()->format('Y-m-d H:i:s')
         ], true);
