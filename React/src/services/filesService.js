a936b2aaf7f314392f10edd269d254898795fdb1
Sebastian <cancrosebastian@gmail.com>
2025-10-05 22:07:54 -0300

    Fix de creacion de noticias

--- React/src/services/filesService.js
+++ React/src/services/filesService.js
@@ -1,7 +1,9 @@
 import { api } from "./api";
 
 export const fileService = {
-    UploadFile : (data) => api.post("/files/upload", data)
+    UploadFile : (data) => api.post("/files/upload", data,
+        { headers: { "Content-Type": "multipart/form-data" } }
+    )
               .then(response => {
                   return response;
               })
