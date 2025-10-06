13fec34bacb56cf115ad2daaa80cb64a0467b4ae
Sebastian <cancrosebastian@gmail.com>
2025-10-06 20:14:42 -0300

    Acomodados archivos sql en uno solo

--- API/base.sql
+++ API/base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: mysql:3306
--- Tiempo de generación: 17-09-2025 a las 22:24:00
+-- Tiempo de generación: 06-10-2025 a las 23:14:00
 -- Versión del servidor: 5.7.44
 -- Versión de PHP: 8.2.29
 
@@ -32,8 +32,8 @@ CREATE TABLE `articles` (
   `title` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `body` varchar(255) NOT NULL,
-  `deleted` tinyint(1) NOT NULL
+  `body` longtext NOT NULL,
+  `deleted` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,16 +45,17 @@ CREATE TABLE `articles` (
 CREATE TABLE `domains` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `code` varchar(255) NOT NULL
+  `code` varchar(255) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `domains`
 --
 
-INSERT INTO `domains` (`id`, `name`, `code`) VALUES
-(1, 'Mi primer dominio', 'domain-1'),
-(2, 'Mi segundo dominio', 'domain-2');
+INSERT INTO `domains` (`id`, `name`, `code`, `deleted`) VALUES
+(1, 'Mi primer dominio', 'domain-1', 0),
+(2, 'Mi segundo dominio', 'domain-2', 0);
 
 -- --------------------------------------------------------
 
@@ -70,15 +71,18 @@ CREATE TABLE `users` (
   `token` varchar(255) DEFAULT NULL,
   `token_auth_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `role` varchar(255) NOT NULL,
-  `is_Active` tinyint(1) NOT NULL
+  `is_active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `token_auth_date`, `role`, `is_Active`) VALUES
-(3, 'usuario de prueba', 'test@mail.com', '$2y$10$Hjx7widDGWM12ehqQok55O2Mtrk5f98/7udD5/R0xLvbbgO0AhnHS', 'e42aada057e4024ac5d75b88b5a26c6c', '2025-09-17 20:21:13', 'visitor', 1);
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `token_auth_date`, `role`, `is_active`) VALUES
+(3, 'usuario de prueba', 'test@mail.com', '', NULL, NULL, 'super_adm', 1),
+(4, 'Molo', 'MoloKlaus27@gmail.com', '$2y$10$I5s8MPteQb/uEgyD.CHpBOTwEvXruHKRMK4rK3T/Gl9k3RiMVrIl.', '3d4d52e64def0978606cbf07027ba021', '2025-10-02 10:22:59', 'super_adm', 1),
+(10, 'natalia', 'natalia@bibloteca.com', '$2y$10$FYtYa9NnwpBIAOKYset2I.DuMFu9lGbdw2AGw9Wj83.LBTvgFjYVS', '317b469ce0eb233c8dcda5e0d784b362', '2025-10-02 08:48:23', 'visitor', 1),
+(11, 'Sebastian', 'sebastian@mail.com', '$2y$10$CWi4.UftmUPC0UbuiDlUWOKaZPTI6Ls//GD5uzRtMQ83hueBY396m', 'a3d65359b82c05b04cd849175a9e49a2', '2025-10-06 21:08:08', 'super_adm', 1);
 
 --
 -- Índices para tablas volcadas
@@ -110,7 +114,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de la tabla `domains`
@@ -122,109 +126,7 @@ ALTER TABLE `domains`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Servidor: mysql:3306
--- Tiempo de generación: 23-09-2025 a las 14:43:57
--- Versión del servidor: 5.7.44
--- Versión de PHP: 8.2.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `hexagonal`
---
-
--- --------------------------------------------------------
-
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Servidor: mysql:3306
--- Tiempo de generación: 01-10-2025 a las 13:10:11
--- Versión del servidor: 5.7.44
--- Versión de PHP: 8.2.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `hexagonal`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `events`
---
-
-CREATE TABLE `events` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `is_Active` tinyint(1) NOT NULL,
-  `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `events`
---
-
-INSERT INTO `events` (`id`, `title`, `description`, `image`, `end_date`, `is_Active`, `deleted`) VALUES
-(7, 'Titulo 1', '21hs.\r\nLunes Miercoles y Viernes.\r\n', '', '2025-09-24', 1, 0),
-(8, 'Titulo 2', '15hs a 16hs\r\nLunes y Viernes', '', '2025-09-24', 1, 0),
-(9, 'tiutlo 3', '21hs', '', '2025-09-20', 1, 0),
-(10, 'titulo 4', 'lunes', '', '2025-09-22', 1, 0),
-(11, 'titulo', 'descripcion 1', '', '2025-09-18', 1, 0);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `events`
---
-ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
